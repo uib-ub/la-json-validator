cc6106d43f232ea7114c33f361e6cda55adfdd32
Rob Sanderson <azaroth@gmail.com>
2024-12-17 16:05:46 -0500

    update for 1.0

--- make-docs.py
+++ make-docs.py
@@ -1,18 +1,21 @@
 
 import os
-from json_schema_for_humans import generate
-from json_schema_for_humans.generate import GenerationConfiguration, generate_from_filename, TEMPLATE_FILE_NAME
+from json_schema_for_humans.generate import GenerationConfiguration, generate_from_filename
+from json_schema_for_humans.generation_configuration import GenerationConfiguration
 
-config = GenerationConfiguration(
-	show_breadcrumbs=False,
-	description_is_markdown=True,
-	template_folder="./template",
-	template_name="la")
-TEMPLATE_FILE_NAME = "base_nohtml.html"
-generate.TEMPLATE_FILE_NAME = TEMPLATE_FILE_NAME
+#config = GenerationConfiguration(
+#	show_breadcrumbs=False,
+#	description_is_markdown=True,
+#	template_folder="./template",
+#	template_name="la")
+#TEMPLATE_FILE_NAME = "base_nohtml.html"
+#generate.TEMPLATE_FILE_NAME = TEMPLATE_FILE_NAME
 
-entries = ['object', 'place', 'person', 'group', 'set', 'text', 'image', 'provenance', 'event', 'digital']
 
+config=GenerationConfiguration(show_breadcrumbs=False, description_is_markdown=False,custom_template_path="template/la/base_nohtml.html")
+
+
+entries = ['object', 'place', 'person', 'group', 'set', 'text', 'image', 'provenance', 'event', 'digital', 'abstract', 'concept']
 header = """---
 title: "Linked Art Schema: {{title}}"
 up_href: "/api/1.0/"
@@ -31,8 +34,3 @@ for s in entries:
 	fh = open(f'html/{s}.html', 'w')
 	fh.write(out)
 	fh.close()
-
-# Can GET info from https://spinup.internal.yale.edu/api/v2/storage/5446
-#os.system("aws s3 --profile linked-data.yalespace.org sync html s3://linked-data.yalespace.org/docs/model/")
-#os.system("aws cloudfront --profile linked-data.yalespace.org create-invalidation --distribution-id E1CKFKVC1UT9DM --paths \"/*\"")
-
